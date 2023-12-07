6e5afa42b51fe5d3c11cc0c4b317a4757cfc00bd
mackjmr <mackenzie.jomard@datadoghq.com>
2023-12-07 18:47:57 +0100

    update type

--- jmx-metrics/src/main/resources/target-systems/kafka.groovy
+++ jmx-metrics/src/main/resources/target-systems/kafka.groovy
@@ -88,12 +88,12 @@ otel.instrument(requestTime,
   "99thPercentile", otel.&doubleValueCallback)
 otel.instrument(requestTime,
   "kafka.request.time.avg",
-  "The total time the broker has taken to service requests",
+  "The avg time the broker has taken to service requests",
   "ms",
   [
     "type" : { mbean -> mbean.name().getKeyProperty("request").toLowerCase() },
   ],
-  "Mean", otel.&longCounterCallback)
+  "Mean", otel.&doubleValueCallback)
 
 def network = otel.mbeans(["kafka.server:type=BrokerTopicMetrics,name=BytesInPerSec",
                           "kafka.server:type=BrokerTopicMetrics,name=BytesOutPerSec"])
@@ -186,10 +186,10 @@ def requestQueueSize = otel.mbean("kafka.network:type=RequestChannel,name=Reques
 otel.instrument(requestQueueSize, "kafka.request.queue", "size of the request queue",
         "{requests}", "Value", otel.&longValueCallback)
 
-def logFlushRate = otel.mbean("kafka.log:type=LogFlushStats,name=LogFlushRateAndTimeMs")
-otel.instrument(logFlushRate, "kafka.logs.flush.time.count", "log flush count",
-        "ms", "Count", otel.&longCounterCallback)
-otel.instrument(logFlushRate, "kafka.logs.flush.time.median", "log flush time - 50th percentile",
-        "ms", "50thPercentile", otel.&doubleValueCallback)
-otel.instrument(logFlushRate, "kafka.logs.flush.time.99p", "log flush time - 99th percentile",
-        "ms", "99thPercentile", otel.&doubleValueCallback)
+// def logFlushRate = otel.mbean("kafka.log:type=LogFlushStats,name=LogFlushRateAndTimeMs")
+// otel.instrument(logFlushRate, "kafka.logs.flush.time.count", "log flush count",
+//         "ms", "Count", otel.&longCounterCallback)
+// otel.instrument(logFlushRate, "kafka.logs.flush.time.median", "log flush time - 50th percentile",
+//         "ms", "50thPercentile", otel.&doubleValueCallback)
+// otel.instrument(logFlushRate, "kafka.logs.flush.time.99p", "log flush time - 99th percentile",
+//         "ms", "99thPercentile", otel.&doubleValueCallback)
